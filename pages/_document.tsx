be0ea3093016ef1b75adb528a046a0321b2722cf
Adrian Pilarczyk <adrian@useo.pl>
2020-12-12 18:26:06 +0100

    Add _document with meta tags

---
+++ pages/_document.tsx
@@ -0,0 +1,31 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta charSet="utf-8" />
+          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+          <meta
+            name="viewport"
+            content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+          />
+          <meta name="description" content="Description" />
+          <meta name="keywords" content="Keywords" />
+          <title>Teacher Classroom PWA</title>
+
+          <link rel="manifest" href="/manifest.json" />
+          <link href="/favicon-16x16.png" rel="icon" type="image/png" sizes="16x16" />
+          <link href="/favicon-32x32.png" rel="icon" type="image/png" sizes="32x32" />
+          <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+          <meta name="theme-color" content="#317EFB" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
